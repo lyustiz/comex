a5e46358b28fa54149b7d1c2417fa36102da46a3
lyustiz <you@example.com>
2020-02-01 17:33:58 -0400

    feat: mock data sucursal

---
+++ src/app/data/service/sucursal.ts
@@ -0,0 +1,18 @@
+export interface Sucursal {
+  id:	number;
+  co_sucursal:	number;
+  nu_sucursal:	string;
+  tx_glosa_sucursal:	string;
+  tx_direccion:	string;
+  nu_direccion:	number;
+  tx_comuna:	string;
+  tx_telefono:	string;
+  tx_fax:	string;
+  nu_flg_Idc_banco:	number;
+  nu_banco:	number;
+  tx_email_agente:	string;
+  id_usuario:	number;
+  id_status:	number;
+  fe_creado:	string;
+  fe_actualizado:	string;
+}
