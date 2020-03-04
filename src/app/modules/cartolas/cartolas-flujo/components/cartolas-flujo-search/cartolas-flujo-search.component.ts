a62593609f7c84787ff8e5edf3fedc11e12d2ea9
lyustiz <lyustiz@gmail.com>
2020-03-04 11:59:46 -0400

    implement serach and dinamic component

--- src/app/modules/cartolas/cartolas-flujo/components/cartolas-flujo-search/cartolas-flujo-search.component.ts
+++ src/app/modules/cartolas/cartolas-flujo/components/cartolas-flujo-search/cartolas-flujo-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators as cValidators} from '@shared/utils/validators';
 @Component({
   selector: 'app-cartolas-flujo-search',
   templateUrl: './cartolas-flujo-search.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartolasFlujoSearchComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup;
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      cliRut:	      [ '', [ Validators.required, cValidators.rut ] ],     // RUT
+      procod:	      [ '', [ Validators.required] ],     // RUT
+      prosub:	      [ '', [ Validators.required] ],     // RUT
+      eveedo:	      [ '', [ Validators.required] ],     // RUT
+      fecini:	      [ '', [ Validators.required] ],     // RUT
+      fecfin:	      [ '', [ Validators.required] ],     // RUT
+      refbco:	      [ '', [ Validators.required] ],     // RUT
+      cliref:	      [ '', [ Validators.required] ],     // RUT
+    });
   }
+
 
 }
