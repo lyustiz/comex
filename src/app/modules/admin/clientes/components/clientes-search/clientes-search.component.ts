3482ff15aa3b543d4b198fcd08ba3a4ff6ab2fc5
lyustiz <lyustiz@gmail.com>
2020-03-06 16:25:21 -0400

    update search

--- src/app/modules/admin/clientes/components/clientes-search/clientes-search.component.ts
+++ src/app/modules/admin/clientes/components/clientes-search/clientes-search.component.ts
@@ -1,20 +1,23 @@
-import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef, Input, OnInit, ReflectiveInjector } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef, AfterViewInit, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomValidators as cValidators} from '@shared/utils/validators';
+
+import { ClientesListComponent } from '../clientes-list/clientes-list.component';
 
 @Component({
   selector: 'app-clientes-search',
   templateUrl: './clientes-search.component.html',
   styleUrls: ['./clientes-search.component.scss']
 })
-export class ClientesSearchComponent implements OnInit {
-
+export class ClientesSearchComponent implements OnInit, AfterViewInit {
+  
   public formGroup: FormGroup;
 
   public currentComponent = null;
 
- /* @ViewChild('dynamicComponentContainer', { read: ViewContainerRef, static: false}) dynamicComponentContainer: ViewContainerRef;
-  @Input() set componentData(data: {component: any, inputs: any}) {
+  @ViewChild('ComponentContainer', { read: ViewContainerRef, static: false}) dynamicComponentContainer: ViewContainerRef;
+  
+  /*@Input() set componentData(data: {component: any, inputs: any}) {
     if (!data) {
       return;
     }
@@ -34,7 +37,9 @@ export class ClientesSearchComponent implements OnInit {
 
   }*/
 
-  constructor(public formBuilder: FormBuilder) { }
+  constructor(public formBuilder: FormBuilder,
+              public componentFactoryResolver: ComponentFactoryResolver,
+              /* */) { }
 
   ngOnInit() {
 
@@ -42,7 +47,18 @@ export class ClientesSearchComponent implements OnInit {
       cliRut:	      [ '', [ Validators.required, cValidators.rut ] ],     // RUT
       cliNom:	      [ '', [ Validators.required] ],                       // NOMBRE
     });
+
+
   }
+
+  ngAfterViewInit(): void {
+    console.log(this.dynamicComponentContainer);
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(ClientesListComponent);
+    const viewContainerRef = this.dynamicComponentContainer;
+    viewContainerRef.clear();
+    viewContainerRef.createComponent(componentFactory);
+  }
+
 
   formField(field: string) {
     return this.formGroup.get(field);
